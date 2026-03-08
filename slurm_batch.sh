db79de73c1fee2749837919fcb584d445631a8a5
Siddharth M. Bhatia <siddharth@sidmb.com>
2026-03-07 18:13:41 -0800

    updated slurm commands

--- slurm_batch.sh
+++ slurm_batch.sh
@@ -5,4 +5,14 @@
 export HF_TOKEN=$(cat ~/.shell/secrets/hf_token)
 
 # Submit batch
-sbatch ./slurm_run_debug.sh "$@"
+sbatch \
+  --account=nlp \
+  --gres=gpu:1 \
+  --constraint=48G \
+  --mem=128G \
+  --partition=jag-standard \
+  --job-name=gemma2_2b \
+  --time=21-00:00:00 \
+  --mail-user="$USER@cs.stanford.edu" \
+  --mail-type=END,FAIL \
+  ./slurm_run_debug.sh "$@"
