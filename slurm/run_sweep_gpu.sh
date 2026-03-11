b269d694d7bbd22201fb3239a32cfb455a400c8b
Siddharth M. Bhatia <siddharth@sidmb.com>
2026-03-10 21:22:14 -0700

    default sweeps 4

--- slurm/run_sweep_gpu.sh
+++ slurm/run_sweep_gpu.sh
@@ -12,5 +12,5 @@ source slurm/common.sh
 run ./run_sweep.sh \
     --config training/configs/gemma2_2b.yaml \
     --sweep training/configs/sweeps/lr.yaml \
-    --sweep_count 3 \
+    --sweep_count 4 \
     "$@"
