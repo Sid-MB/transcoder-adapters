db150f36c7ec0be521ee542a117ececf15404a7d
Siddharth M. Bhatia <siddharth@sidmb.com>
2026-04-22 21:27:30 -0700

    added visualize_features script for easy use

---
+++ sh/visualize_features.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+
+# Run the feature dashboard
+uv run python -m analysis.features.visualize.feature_dashboard --data_dir /nlp/scr/siddharth/sparse-adaptation/feature_data/2026.TA.gemma2_2b_tc8192_decb_l1w0.001_tarbb_lb2.0_ln1_dr20000_lr8e-04_bs4_sl147_20260416_151353_15175430
