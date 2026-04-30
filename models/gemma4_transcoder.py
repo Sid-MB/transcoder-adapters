24a1e9c39a629333dcb6daaa5d8ddc05c8e967cf
Siddharth M. Bhatia <siddharth@ai.stanford.edu>
2026-04-30 05:00:56 -0700

    Gemma model fixes

--- models/gemma4_transcoder.py
+++ models/gemma4_transcoder.py
@@ -10,6 +10,14 @@ from transformers import Gemma4ForCausalLM, Gemma4TextConfig
 from transformers.models.gemma4.modeling_gemma4 import Gemma4TextMLP
 
 
+_GEMMA4_UNUSED_MULTIMODAL_KEY_PATTERNS = [
+    r"^model\.audio_tower\.",
+    r"^model\.embed_audio\.",
+    r"^model\.embed_vision\.",
+    r"^model\.vision_tower\.",
+]
+
+
 class Gemma4ConfigWithTranscoder(Gemma4TextConfig):
     """Gemma4 config with transcoder parameters."""
 
@@ -100,6 +108,10 @@ class Gemma4ForCausalLMWithTranscoder(Gemma4ForCausalLM):
 
     def __init__(self, config):
         super().__init__(config)
+        self._keys_to_ignore_on_load_unexpected = [
+            *(self._keys_to_ignore_on_load_unexpected or []),
+            *_GEMMA4_UNUSED_MULTIMODAL_KEY_PATTERNS,
+        ]
         for layer_idx, layer in enumerate(self.model.layers):
             layer.mlp = Gemma4MLPWithTranscoder(config, layer_idx)
 
