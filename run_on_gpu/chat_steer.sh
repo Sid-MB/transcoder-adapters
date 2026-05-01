79d252b9f5fcb6065ffdbb9ace8675babdad4af0
Siddharth M. Bhatia <siddharth@ai.stanford.edu>
2026-05-01 12:47:02 -0700

    Fixed typo in chat_steer script

--- run_on_gpu/chat_steer.sh
+++ run_on_gpu/chat_steer.sh
@@ -6,5 +6,5 @@
 # Or, with steering
 # ./run_on_gpu/chat_steer.sh siddharthmb/2026.TA.gemma2_2b_tc8192_decb_l1w0.001_tarbb_lb2.0_ln1_dr20000_lr8e-04_bs4_sl14793860 --steer 11551209:4
 
-uv run pythom -m analysis.simple_load.simple_load "$@"
+uv run python -m analysis.simple_load.simple_load "$@"
 
